58477c0e57c85ec4d4c45aeeba736ad241ad4518
will4879 <will4879@gmail.com>
2022-12-26 11:32:11 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,32 @@
+﻿using cadastroDePacientes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace cadastroDePacientes.Controllers
+{
+    public class CadastroController : Controller
+    {
+        private readonly ILogger<CadastroController> _logger;
+
+        public CadastroController(ILogger<CadastroController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Colaboradores()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
