a5b04460cbeb9e431da448f18f522ac7b5a1c052
will4879 <will4879@gmail.com>
2022-12-26 15:16:39 -0300

    adicianodo primeira model , corrigido erros de links

---
+++ Controllers/ListarController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cadastroDePacientes.Controllers
+{
+    public class ListarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Colaboradores()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/PacienteModel.cs
@@ -0,0 +1,18 @@
+﻿namespace cadastroDePacientes.Models
+{
+    public class PacienteModel
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }
+
+        public PacienteModel(string nome, string telefone, string email, string endereco)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            Email = email;
+            Endereco = endereco;
+        }
+    }
+}
